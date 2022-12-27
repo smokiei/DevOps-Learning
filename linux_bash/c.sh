d9c4602c96134642bd8e00b6edfe148f6967ea2e
tkachenko <smokiei473@gmail.com>
2022-12-27 21:33:21 +0200

    initial commit

--- linux_bash/c.sh
+++ linux_bash/c.sh
@@ -19,6 +19,34 @@ function show_usage_and_exit()
     exit 0
 }  
 
+PIDFILE=/var/run/c.pid
+
+if [ -f $PIDFILE ]
+then
+  PID=$(cat $PIDFILE)
+  ps -p $PID > /dev/null 2>&1
+  if [ $? -eq 0 ]
+  then
+    echo "Process already running"
+    exit 1
+  else
+    ## Process not found assume not running
+    echo $$ > $PIDFILE
+    if [ $? -ne 0 ]
+    then
+      echo "Could not create PID file"
+      exit 1
+    fi
+  fi
+else
+  echo $$ > $PIDFILE
+  if [ $? -ne 0 ]
+  then
+    echo "Could not create PID file"
+    exit 1
+  fi
+fi
+
 if [ -z "$1" ] || [ -z "$2" ]; then
     show_usage_and_exit
 fi
